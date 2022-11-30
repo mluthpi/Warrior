cbb25f8990c4087b4e98655de7e3030f236c426a
muhammad Luthfiyanto <luthfiyantomuhammad@gmail.com>
2022-11-30 14:23:59 +0700

    adjust margin

--- app/src/main/java/com/example/warrior/LoginActivity.kt
+++ app/src/main/java/com/example/warrior/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.warrior
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.warrior.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +13,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        binding.btnLanjutkan.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            if (email == "abc" && password == "123") {
+                Toast.makeText(this,email, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "salah", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/warrior/RegisterActivity.kt
+++ app/src/main/java/com/example/warrior/RegisterActivity.kt
@@ -2,10 +2,15 @@ package com.example.warrior
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.warrior.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
